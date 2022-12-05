b6a1eb187445fec561d8b610705589a28a816a91
Genário Carneiro <53302984+genariocoliveira@users.noreply.github.com>
2022-12-05 16:13:46 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,90 +3,74 @@ const btnReset = document.getElementById('btn-reset')
 const valorPrincipal = document.getElementById('valor-principal').value
 const numberPerson = document.getElementById('n-people').value
 
+
 btnReset.addEventListener('click', () => {
   valorPrincipal.innerText = '0'
   valorGorjeta.innerHTML = '0'
   valorTotal.innerHTML = '0'
 })
 
-
-
 function calcular(tipo, valor) {
   const valorPrincipal = document.getElementById('valor-principal').value
   const numberPerson = document.getElementById('n-people').value
   const valueAmount = document.getElementById('valorGorjeta')
   const valueTotal = document.getElementById('valorTotal')
-  const modal = document.getElementById('my-modal')
+  const alertaPrincipal = document.querySelector('#alert-value')
+  const borP = document.querySelector('#borda-valor-principal')
   console.log(tipo, valor)
 
   if (valorPrincipal == '') {
-    
+    alertaPrincipal.classList.add('info-ativ')
+    borP.classList.add('input-ativ')
   } else if (valor === 5) {
     resFivePercent = (valorPrincipal / 100) * 5
     total = valorPrincipal - resFivePercent
-    if (numberPerson == 0) {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / 1).toFixed(2)
-    } else {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / numberPerson).toFixed(2)
-    }
-
+    
+    alertaPrincipal.classList.remove('info-ativ')
+    borP.classList.remove('input-ativ')
+    valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
+    valueTotal.innerHTML = (total / numberPerson).toFixed(2)
   }
   else if (valor === 10) {
     resFivePercent = (valorPrincipal / 100) * 10
     total = valorPrincipal - resFivePercent
-    if (numberPerson == 0) {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / 1).toFixed(2)
-    } else {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / numberPerson).toFixed(2)
-    }
+    
+    alertaPrincipal.classList.remove('info-ativ')
+    borP.classList.remove('input-ativ')
+    valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
+    valueTotal.innerHTML = (total / numberPerson).toFixed(2)
   }
   else if (valor === 15) {
     resFivePercent = (valorPrincipal / 100) * 15
     total = valorPrincipal - resFivePercent
-    if (numberPerson == 0) {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / 1).toFixed(2)
-    } else {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / numberPerson).toFixed(2)
-    }
+    alertaPrincipal.classList.remove('info-ativ')
+    borP.classList.remove('input-ativ')
+    valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
+    valueTotal.innerHTML = (total / numberPerson).toFixed(2)
   }
   else if (valor === 25) {
     resFivePercent = (valorPrincipal / 100) * 25
     total = valorPrincipal - resFivePercent
-    if (numberPerson == 0) {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / 1).toFixed(2)
-    } else {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / numberPerson).toFixed(2)
-    }
+    alertaPrincipal.classList.remove('info-ativ')
+    borP.classList.remove('input-ativ')
+    valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
+    valueTotal.innerHTML = (total / numberPerson).toFixed(2)
   }
   else if (valor === 50) {
     resFivePercent = (valorPrincipal / 100) * 50
     total = valorPrincipal - resFivePercent
-    if (numberPerson == 0) {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / 1).toFixed(2)
-    } else {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / numberPerson).toFixed(2)
-    }
+    alertaPrincipal.classList.remove('info-ativ')
+    borP.classList.remove('input-ativ')
+    valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
+    valueTotal.innerHTML = (total / numberPerson).toFixed(2)
   }
   else if (valor === '') {
     const custom = document.getElementById('custom').value
     resFivePercent = (valorPrincipal / 100) * custom
     total = valorPrincipal - resFivePercent
-    if (numberPerson == 0) {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / 1).toFixed(2)
-    } else {
-      valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
-      valueTotal.innerHTML = (total / numberPerson).toFixed(2)
-    }
+    alertaPrincipal.classList.remove('info-ativ')
+    borP.classList.remove('input-ativ')
+    valueAmount.innerHTML = ((total / 100) * 5).toFixed(2)
+    valueTotal.innerHTML = (total / numberPerson).toFixed(2)
   }
 }
